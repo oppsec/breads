6af7854c36f03f781b4dc9c5c5d491ce4323a6de
oppsec <opps3c@gmail.com>
2024-03-07 15:08:22 -0300

    release: breads 1.1.9*

--- src/ui/banner.py
+++ src/ui/banner.py
@@ -2,7 +2,7 @@ from random import choice
 from rich.console import Console
 console = Console()
 
-VERSION = '1.1.8'
+VERSION = '1.1.9'
 BANNER = f'''
 BREADS :: Breaking Active Directory Security :: {VERSION}
 TIP: Use 'help' to list all modules
