c586289f45817300f181137df434b821cf458564
oppsec <opps3c@gmail.com>
2024-03-15 16:24:41 -0300

    new: added SID objectType identification

--- src/modules/ldap/sid.py
+++ src/modules/ldap/sid.py
@@ -13,15 +13,18 @@ class Sid:
     multiple_hosts = False
     user_target = None
     requires_args = True
-    attributes = ['*']
+    attributes = ['objectClass']
     min_args = 1
 
-    def __init__(self, context=None, module_options=None):
-        self.context = context
-        self.module_options: Optional[Dict] = module_options
+    def process_info(self, conn, base_dn, search_filter, attribute_list):
+        search = conn.search(base_dn, search_filter, attributes=attribute_list)
+        search_response = search[2]
 
-    def options (self):
-        pass
+        for entry in search_response:
+                if entry['type'] == 'searchResEntry':
+                    attributes = entry['attributes']
+                    for attr, value in attributes.items():
+                        console.print(f" - [cyan]{attr}[/]: {value}", highlight=False)
 
     def on_login(self, sid: str) -> None:
 
@@ -36,12 +39,34 @@ class Sid:
         results = conn.search(base_dn, search_filter, attributes=self.attributes)
         res_status = results[0]
         res_response = results[2]
+        sid_info = {}
+        seen_attributes = set()
 
         if res_status:
             console.print("[green][+][/] SID Information:")
             for entry in res_response:
                 if entry['type'] == 'searchResEntry':
-                    for key, value in entry['attributes'].items():
-                        console.print(f"[green][+][/] {key}: {value}", highlight=False)
+                    for attribute, value in entry['attributes'].items():
+                        if attribute not in seen_attributes:
+                            sid_info[attribute] = value
+                            seen_attributes.add(attribute)
+
+            objectClass_value = sid_info.get('objectClass', [])
+            console.print(f'[yellow italic] \_ objectClass: {objectClass_value}[/]')
+
+            if 'computer' in objectClass_value:
+                computer_attrs = ['cn', 'distinguishedName', 'memberOf', 'objectSid', 'operatingSystem', 'dNSHostName']
+                self.process_info(conn, base_dn, search_filter, computer_attrs)
+
+            elif 'user' in objectClass_value:
+                user_attrs = ['cn', 'description', 'distinguishedName', 'memberOf', 'whenCreated', 'name', 'userAccountControl', 'badPwdCount', 'lastLogon', 'objectSid', 'sAMAccountName']
+                self.process_info(conn, base_dn, search_filter, user_attrs)
+
+            elif 'group' in objectClass_value:
+                group_attrs = ['cn', 'member', 'distinguishedName', 'whenCreated', 'memberOf', 'objectGUID', 'objectSid']
+                self.process_info(conn, base_dn, search_filter, group_attrs)
+            else:
+                console.print("[yellow] objectClass type not identified, returning all attributes.[/]")
+                self.process_info(conn, base_dn, search_filter, ['*'])
         else:
             console.print("[red][!][/] No entries found in the results.")
