190f169a427c835a8f6ccfe9a09279a7e40a72de
oppsec <opps3c@gmail.com>
2024-03-25 12:19:15 -0300

    fix: fixed ssl problem

--- src/handlers/ldap_connection.py
+++ src/handlers/ldap_connection.py
@@ -1,13 +1,16 @@
+from ldap3 import Server, Connection, ALL, NTLM, SAFE_SYNC
 from pathlib import Path
-import json
+from json import load
 from rich.console import Console 
-console = Console()
-from ldap3 import Server, Connection, ALL, NTLM, KERBEROS, SAFE_SYNC
 
 from handlers.profile.helper import get_current_profile, BREADS_FOLDER
 BREADS_FOLDER = Path(BREADS_FOLDER)
 
+console = Console()
+
 class LdapHandler:
+    """ BREAD's default LDAP handler class """
+
     def __init__(self):
         self.domain = ""
         self.password = ""
@@ -15,31 +18,40 @@ class LdapHandler:
         self.hostname = ""
 
     def connection(self):
+        """ Default LDAP connection handler """
+    
         if get_current_profile() == 'None':
             console.print("[red][!][/] You need to load a profile first, use 'load_profile' command")
             return None, None
         
-        settings_json_file = f"{BREADS_FOLDER}/{get_current_profile()}/settings.json"
-
-        with open(settings_json_file, 'r') as settings_file:
-            data = json.load(settings_file)
+        prifle_json = f"{BREADS_FOLDER}/{get_current_profile()}/settings.json"
+        with open(prifle_json, 'r') as settings_file:
+            data = load(settings_file)
 
             self.username = data['username']
             self.hostname = data['host']
             self.password = data['password']
             self.domain = data['domain']
+
         try:
             server = Server(f"ldaps://{self.hostname}", use_ssl=True, get_info=ALL)
+
+            if not(server.ssl):
+                server = Server(f"ldap://{self.hostname}", use_ssl=False, get_info=ALL)
+
             conn = Connection(server, user=self.username, password=self.password, authentication=NTLM, client_strategy=SAFE_SYNC, auto_bind=True)
             base_dn = server.info.other['rootDomainNamingContext'][0]
 
             return conn, base_dn
+        
         except Exception as error:
-            console.print(f"[red][!][/] [bright_white]LDAP Error: {error}")
+            console.print(f"[red][!][/] Failed to authenticate to {self.domain} Active Directory: {error}")
+            #raise Exception
             return None, None
         
     def modify_entry(self, dn, mod_attrs):
-        """Modifies an LDAP entry with the given attributes."""
+        """ Modifies an LDAP entry with the given attributes """
+        
         try:
             connect = self.connection() 
             connect.modify_s(dn, mod_attrs)
