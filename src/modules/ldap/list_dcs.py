067c0e1bbf4bda499a9e8967e912d221591471c6
oppsec <opps3c@gmail.com>
2024-04-12 03:01:20 -0300

    fix: Removed unused code from 'list_dcs' module

--- src/modules/ldap/list_dcs.py
+++ src/modules/ldap/list_dcs.py
@@ -10,37 +10,20 @@ class ListDcs:
     module_protocol = ["ldap"]
     opsec_safe = True
     multiple_hosts = False
-    search_filter = (
-        "(&(objectCategory=Computer)(userAccountControl:1.2.840.113556.1.4.803:=8192))"
-    )
+    search_filter = "(&(objectCategory=Computer)(userAccountControl:1.2.840.113556.1.4.803:=8192))"
     attributes = "dnsHostname"
-
-    def get_dc_name(self):
-        conn, base_dn = LdapHandler.connection(self)
-
-        if conn is None:
-            console.print("[red][!][/] Failed to establish LDAP connection.")
-            return []
-
-        results = conn.search(base_dn, self.search_filter, attributes=self.attributes)
-        res_response = results[2]
-        dcs_list = []
-
-        for entry in res_response:
-            if entry["type"] == "searchResEntry":
-                hostname = entry["attributes"][self.attributes]
-                dcs_list.append(hostname)
-        return dcs_list
 
     def on_login(self):
         conn, base_dn = LdapHandler.connection(self)
         results = conn.search(base_dn, self.search_filter, attributes=self.attributes)
         res_status = results[0]
         res_response = results[2]
+
         dcs_list = []
 
         if res_status:
             console.print("[green][+][/] Domain Controllers:")
+
             for entry in res_response:
                 if entry["type"] == "searchResEntry":
                     hostname = entry["attributes"][self.attributes]
