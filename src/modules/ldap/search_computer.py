bdfc8b5f0357a34847767505ddc98734ca3b491f
Daniel <50470310+oppsec@users.noreply.github.com>
2024-05-27 20:54:23 -0300

    test: new module search_computer.py

---
+++ src/modules/ldap/search_computer.py
@@ -0,0 +1,34 @@
+# Test
+
+from rich.console import Console
+from handlers.ldap_connection import LdapHandler
+
+console = Console()
+
+class SearchComputer:
+    name = "search_computer"
+    desc = "Search for all computers that have the specified word in the CN attribute"
+    module_protocol = ["ldap"]
+    opsec_safe = True
+    multiple_hosts = False
+    search_filter = None
+    attributes = "sAMAccountName"
+    requires_args = True
+    min_args = 1
+    usage_desc = "[yellow]Usage:[/] search_computer <word> (ex: search_computer PC)"
+
+    def on_login(self, user_input):
+        search_filter = f"(&(objectCategory=computer)(cn=*{user_input}*))"
+
+        conn, base_dn = LdapHandler.connection(self)
+        results = conn.search(base_dn, search_filter, attributes=self.attributes)
+        res_status = results[0]
+        res_response = results[2]
+
+        if res_status:
+            console.print(f"[green][+][/] Searching for computers with '{user_input}' in the name:")
+
+            for entry in res_response:
+                console.print(res_response)
+        else:
+            console.print("[red][!][/] No entries found in the results.")
